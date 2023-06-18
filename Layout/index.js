f1a82faa0622def7e345108e01797f306f8b8b7d
Joyce <caroline.joyce5631@gmail.com>
2023-06-18 17:40:09 -0300

    :hammer: Função: adicionar e remover item da lista

--- Layout/index.js
+++ Layout/index.js
@@ -3,12 +3,23 @@ function AddItemDay () {
 }
 
 function RemoveItemDay () {
-    document.getElementById("items-list-day").remove(lastChild);
+    let item = document.getElementById("items-list-day");
+    console.log(item.lastChild)
 
+    if(item.parentNode){
+        item.lastChild.remove();
+    }
 }
 
 function AddItemNight () {
     document.getElementById("items-list-night").innerHTML += '<li  class="items-list">Lorem ipsum dolor sit amet consectetur.</li>'
 }
 
+function RemoveItemNight () {
+    let item = document.getElementById("items-list-night");
+    console.log(item.lastChild)
 
+    if(item.parentNode){
+        item.lastChild.remove();
+    }
+}

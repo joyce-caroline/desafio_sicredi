3177eaa52098df36db30821a5adf41838600e4aa
Joyce <caroline.joyce5631@gmail.com>
2023-06-18 16:44:21 -0300

    :hammer: Estilização do layout e add de button

---
+++ Layout/index.js
@@ -0,0 +1,14 @@
+function AddItemDay () {
+    document.getElementById("items-list-day").innerHTML += '<li  class="items-list">Lorem ipsum dolor sit amet consectetur.</li>'
+}
+
+function RemoveItemDay () {
+    document.getElementById("items-list-day").remove(lastChild);
+
+}
+
+function AddItemNight () {
+    document.getElementById("items-list-night").innerHTML += '<li  class="items-list">Lorem ipsum dolor sit amet consectetur.</li>'
+}
+
+
